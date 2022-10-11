656a1fd37a8b135947f17d6c4bb3828fa70772d4
EC2 Default User <ec2-user@ip-172-31-47-113.ap-northeast-1.compute.internal>
2022-10-11 10:42:40 +0000

    [Start]task

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,22 @@
 class BooksController < ApplicationController
   def index
     @book = Book.new
+    @user_id = User.find(current_user.id)
+    @user_image = User.find(current_user.id)
   end
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to book_path
+    redirect_to users_path
+  end
+  def show
   end
 
   private
 
   def book_params
-    params.require(book).permit(:title, :body, :user_id)
+    params.permit(:title, :body, :user_id)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,28 @@
 class UsersController < ApplicationController
   def index
-    @user = User.new
+     @book = Book.new
+     @user_id = User.find(current_user.id)
+     @user_image = User.find(current_user.id)
   end
   def create
-    @user = User.new(user_params)
-    @user.user_id = current_user.id
+    @book = Book.new(user_params)
+    @book.user_id = current_user.id
     @user.save
-    redirect_to user_path
+    redirect_to users_path(current_user.id)
   end
 
   def show
    @user = User.find(params[:id])
   end
 
+  def edit
+     @user = User.find(params[:id])
+  end
+
   private
 
-  def book_params
-    params.require(book).permit(:title, :body, :user_id)
+  def user_params
+    params.require(user).permit(:name, :introduction, :profile_image)
   end
 
 end
